b2dea192d68355b1e8f93fb6a349910bb5d1ba23
hikaricp <jiyongguang0723@gmail.com>
2017-12-13 18:50:57 +0800

    配置后台商品图片的springmvc上传与富文本上传ftp服务器功能开发

--- src/main/java/com/mmall/service/IProductService.java
+++ src/main/java/com/mmall/service/IProductService.java
@@ -3,7 +3,7 @@ package com.mmall.service;
 import com.github.pagehelper.PageInfo;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.Product;
-import com.mmall.vo.ProductDetailVo;
+import com.mmall.vo.ProductDetailVO;
 
 /**
  * @author : Ji YongGuang.
@@ -13,12 +13,17 @@ public interface IProductService {
 
     ServerResponse saveOrUpdateProduct(Product product);
 
-    ServerResponse setSaleStatus(Integer productId, Integer status);
+    ServerResponse<String> setSaleStatus(Integer productId, Integer status);
 
-    ServerResponse<ProductDetailVo> manageProductDetail(Integer productId);
+    ServerResponse<ProductDetailVO> manageProductDetail(Integer productId);
 
-    ServerResponse<PageInfo> getProductList(Integer pageNum, Integer pageSize);
+    ServerResponse<PageInfo> getProductList(int pageNum, int pageSize);
 
-    ServerResponse<PageInfo> searchProduct(String productName, Integer productId, Integer pageNum, Integer
+    ServerResponse<PageInfo> searchProduct(String productName, Integer productId, int pageNum, int
             pageSize);
+
+    ServerResponse<ProductDetailVO> getProductDetail(Integer productId);
+
+    ServerResponse<PageInfo> getProductByKeyWordCategory(String keyword, Integer categoryId, int pageNum,
+                                                         int pageSize, String orderBy);
 }
