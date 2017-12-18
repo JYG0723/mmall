d8fcb8f9837be262697f724f18e4d93b0b84a20a
hikaricp <jiyongguang0723@gmail.com>
2017-12-18 08:15:00 +0800

    新增购物车以及运单的dao层接口

--- src/main/java/com/mmall/dao/ShippingMapper.java
+++ src/main/java/com/mmall/dao/ShippingMapper.java
@@ -1,6 +1,9 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Shipping;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ShippingMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +17,12 @@ public interface ShippingMapper {
     int updateByPrimaryKeySelective(Shipping record);
 
     int updateByPrimaryKey(Shipping record);
+
+    int deleteByShippingIduserId(@Param("userId") Integer userId, @Param("shippingId") Integer shippingId);
+
+    int updateByShipping(Shipping shipping);
+
+    Shipping selectByShippingIdUserId(@Param("userId") Integer userId, @Param("shippingId") Integer shippingId);
+
+    List<Shipping> selectByUserId(Integer userId);
 }
